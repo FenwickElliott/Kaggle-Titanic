cded6a7c639024f09f5657ac8654f3844a99e585
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-12-06 12:39:56 -0500

    DataBase struct

--- main.go
+++ main.go
@@ -21,18 +21,25 @@ func (p *Property) CalculateRatio() {
 	p.ratio = float64(p.survived) / float64(p.total)
 }
 
+type DataBase struct {
+	properties []*Property
+}
+
+func (db *DataBase) addProperty(name string, col int, exp string) {
+	db.properties = append(db.properties, &Property{
+		name: name,
+		col:  col,
+		exp:  exp,
+	})
+}
+
 func main() {
+	db := DataBase{}
+	db.addProperty("male", 4, "male")
+	db.addProperty("female", 4, "female")
+
 	csvFile, _ := os.Open("./train.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
-
-	properties := []*Property{}
-
-	properties = append(properties, &Property{
-		name: "male",
-		col:  4,
-		exp:  "male",
-	})
-
 	for {
 		row, err := reader.Read()
 		if err == io.EOF {
@@ -41,16 +48,17 @@ func main() {
 			fmt.Println(err)
 			fmt.Println(row)
 		} else {
-			for _, p := range properties {
+			for _, p := range db.properties {
 				if row[p.col] == p.exp {
 					p.total++
-					if row[1] == "1" {
-						p.survived++
-					}
+					// if row[1] == "1" {
+					// 	p.survived++
+					// }
 				}
 			}
 		}
 	}
 
-	fmt.Println(properties[0])
+	fmt.Println(db.properties[0])
+	fmt.Println(db.properties[1])
 }
